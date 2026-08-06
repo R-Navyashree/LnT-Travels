7c75e0c058a8c422a23113ca71e26af8c6a1d6e8
R-Navyashree <r.navyasri04@gmail.com>
2026-08-06 15:31:39 +0530

    Redesign website with premium light theme and UI improvements

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,54 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function AnalyticsTracker() {
+  useEffect(() => {
+    const trackPageView = () => {
+      if (typeof window.gtag === 'function') {
+        window.gtag('event', 'page_view', {
+          page_title: document.title,
+          page_location: window.location.href,
+          page_path: window.location.pathname + window.location.search + window.location.hash,
+        })
+      }
+    }
+
+    const timer = window.setInterval(() => {
+      if (typeof window.gtag === 'function') {
+        trackPageView()
+        window.clearInterval(timer)
+      }
+    }, 200)
+
+    const handleRouteChange = () => {
+      trackPageView()
+    }
+
+    window.addEventListener('hashchange', handleRouteChange)
+    window.addEventListener('popstate', handleRouteChange)
+
+    const originalPushState = window.history.pushState
+    window.history.pushState = function (...args) {
+      originalPushState.apply(this, args)
+      handleRouteChange()
+    }
+
+    return () => {
+      window.clearInterval(timer)
+      window.removeEventListener('hashchange', handleRouteChange)
+      window.removeEventListener('popstate', handleRouteChange)
+      window.history.pushState = originalPushState
+    }
+  }, [])
+
+  return null
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AnalyticsTracker />
     <App />
   </StrictMode>,
 )
